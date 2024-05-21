823df04ca17a8be260430fbc5d1a1f58bbc50ce5
sakuyama <sakuyama-sy@tounichi-g.co.jp>
2024-05-21 11:15:41 +0900

    プロジェクト２

--- TeamProject/MyForm.h
+++ TeamProject/MyForm.h
@@ -34,6 +34,8 @@ namespace TeamProject {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Label^ label1;
+	protected:
 
 	private:
 		/// <summary>
@@ -48,11 +50,29 @@ namespace TeamProject {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MyForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(64, 86);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(43, 15);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"label1";
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 15);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(282, 253);
+			this->Controls->Add(this->label1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
 	};
